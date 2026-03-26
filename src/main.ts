3783dc12c0d64375ac09da9b08238afd51ead31b
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2026-03-26 02:48:58 -0300

    feat: add log

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import { FreteModule } from './frete/frete.module';
 async function bootstrap() {
   const app = await NestFactory.create(FreteModule);
   await app.listen(process.env.PORT ?? 3333);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
