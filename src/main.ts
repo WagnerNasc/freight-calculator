f1b8a35ed2d9d89aead78e305f791555f418030b
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2026-03-26 03:17:27 -0300

    fix: changes build

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ import { FreteModule } from './frete/frete.module';
 async function bootstrap() {
   const app = await NestFactory.create(FreteModule);
   await app.listen(process.env.PORT ?? 3333);
-  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
